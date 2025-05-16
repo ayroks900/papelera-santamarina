618e10b727ad37c99432b97415c5621d612be6cc
ayroks900 <m41976626@gmail.com>
2025-05-16 09:12:15 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,30 @@
-# Código principal de Flask para el sitio de Papelera Santamarina
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def inicio():
+    return render_template('index.html')
+
+@app.route('/tienda')
+def tienda():
+    return render_template('tienda.html')
+
+@app.route('/contacto')
+def contacto():
+    return render_template('contacto.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/admin')
+def admin():
+    return render_template('admin.html')
+
+if name == "__main__":
+    app.run(host="0.0.0.0", port=10000)
